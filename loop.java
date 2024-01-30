bfa3616ab50ca5b663d1a971ce6c112c8f8797bf
Huy Nguyen <nlhfz001@gmail.com>
2024-01-30 09:17:35 +0700

    upload loop.java

---
+++ loop.java
@@ -0,0 +1,14 @@
+package bt3.Lab1_B2;
+
+class forexample {
+    /**
+     * @param args
+     */
+    public static void main(String []args)
+    {
+        for(int i = 1;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
